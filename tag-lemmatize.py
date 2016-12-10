0079a4a89c2ad3d3a34d272b7d9580e920ed890d
KT12 <nyy2008@gmail.com>
2016-12-10 17:23:41 -0500

    Finish function

--- tag-lemmatize.py
+++ tag-lemmatize.py
@@ -13,8 +13,9 @@ from nltk import pos_tag
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-# WordNet only cares about 4 parts of speech.
-# The other parts of speech will be encoded as nouns.
+# WordNet only cares about 5 parts of speech.
+# The other parts of speech will be tagged as nouns.
+
 part = {
     'N' : 'n',
     'V' : 'v',
@@ -22,3 +23,27 @@ part = {
     'S' : 's',
     'R' : 'r'
 }
+
+wnl = WordNetLemmatizer()
+
+def convert_tag(penn_tag):
+    '''
+    convert_tag() accepts the **first letter** of a Penn part-of-speech tag,
+    then uses a dict lookup to convert it to the appropriate WordNet tag.
+    '''
+    if penn_tag in part.keys():
+        return part[penn_tag]
+    else:
+        # other parts of speech will be tagged as nouns
+        return 'n'
+
+
+def tag_and_lem(element):
+    '''
+    tag_and_lem() accepts a string, tokenizes, tags, converts tags,
+    lemmatizes, and returns a string
+    '''
+    # list of tuples [('token', 'tag'), ('token2', 'tag2')...]
+    sent = pos_tag(word_tokenize(element)) # must tag in context
+    return ' '.join([wnl.lemmatize(sent[k][0], convert_tag(sent[k][1][0]))
+                    for k in range(len(sent))])
